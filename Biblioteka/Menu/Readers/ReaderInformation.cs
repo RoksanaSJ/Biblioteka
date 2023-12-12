082221694f50488d9c16c61a847795d04e599555
RoksanaSJ <roksana.skinderowicz1@gmail.com>
2023-12-12 15:03:51 +0100

    15:03

--- Biblioteka/Menu/Readers/ReaderInformation.cs
+++ Biblioteka/Menu/Readers/ReaderInformation.cs
@@ -42,7 +42,7 @@ namespace Biblioteka.Menu.Readers
                         Console.WriteLine(borrowing);
                     }
                 }
-
+                break;
             }
         }
     }
